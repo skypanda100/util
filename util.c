a4200479c3dee70217b02abac86b218404cc7394
zhengdongtian <236520700@qq.com>
2019-05-21 15:35:49 +0800

    1. init

---
+++ util.c
@@ -0,0 +1,258 @@
+//
+// Created by zhengdongtian on 4/22/19.
+//
+
+#include "util.h"
+
+int v_system(const char *command)
+{
+    pid_t pid;
+    int status;
+
+    if(command == NULL)
+    {
+        return -1;
+    }
+
+    if((pid = vfork()) < 0)
+    {
+        status = -1;
+    }
+    else if(pid == 0)
+    {
+        execl("/bin/sh", "sh", "-c", command, (char *)0);
+        _exit(127);
+    }
+    else
+    {
+        while(waitpid(pid, &status, 0) < 0)
+        {
+            if(errno != EINTR){
+                status = -1;
+                break;
+            }
+        }
+    }
+
+    return status;
+}
+
+int make_dir(const char *path_ptr)
+{
+    char str[PATH_MAX] = {0};
+    strcpy(str, path_ptr);
+    int len = strlen(str);
+
+    for(int i = 0;i < len;i++)
+    {
+        if(str[i] == '/')
+        {
+            str[i] = '\0';
+            if(strlen(str) == 0)
+            {
+                str[i] = '/';
+                continue;
+            }
+            if(access(str, F_OK) != 0)
+            {
+                if(mkdir(str, 0777) != 0)
+                {
+                    return -1;
+                }
+            }
+            str[i] = '/';
+        }
+    }
+
+    if(len > 0 && access(str, F_OK) != 0)
+    {
+        if(mkdir(str, 0777) != 0)
+        {
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+
+int bz2_decompress(const char *src_file_path_ptr, const char *dst_file_path_ptr)
+{
+    char cmd[1024] = {0};
+    sprintf(cmd, "bzip2 -d -c %s > %s", src_file_path_ptr, dst_file_path_ptr);
+
+    return v_system(cmd);
+}
+
+char *l_trim(const char *input_ptr, char *output_ptr)
+{
+    assert(input_ptr != NULL);
+    assert(output_ptr != NULL);
+    assert(output_ptr != input_ptr);
+    for(;*input_ptr != '\0' && isspace(*input_ptr);++input_ptr)
+    {
+        ;
+    }
+    return strcpy(output_ptr, input_ptr);
+}
+
+char *a_trim(const char *input_ptr, char *output_ptr)
+{
+    char *p = NULL;
+    assert(input_ptr != NULL);
+    assert(output_ptr != NULL);
+    l_trim(input_ptr, output_ptr);
+    for(p = output_ptr + strlen(output_ptr) - 1;p >= output_ptr && isspace(*p);--p)
+    {
+        ;
+    }
+    *(++p) = '\0';
+    return output_ptr;
+}
+
+int reg_exec(const char *input_ptr, const char *pattern_ptr, int *so_ptr, int *eo_ptr)
+{
+    regmatch_t pm[1];
+    size_t nmatch = 1;
+    regex_t rgx;
+
+    if(regcomp(&rgx, pattern_ptr, REG_EXTENDED) != 0)
+    {
+        regfree(&rgx);
+        return -1;
+    }
+
+    if(so_ptr != NULL && eo_ptr != NULL)
+    {
+        if(regexec(&rgx, input_ptr, nmatch, pm, 0) == 0)
+        {
+            *so_ptr = pm[0].rm_so;
+            *eo_ptr = pm[0].rm_eo;
+        }
+        else
+        {
+            *so_ptr = -1;
+            *eo_ptr = -1;
+            regfree(&rgx);
+            return -1;
+        }
+    }
+    else
+    {
+        if(regexec(&rgx, input_ptr, 0, NULL, 0) != 0)
+        {
+            regfree(&rgx);
+            return -1;
+        }
+    }
+
+    regfree(&rgx);
+
+    return 0;
+}
+
+
+void sort_by_ascii(char **c_ptr_ptr, int len, int file_len, int is_desc)
+{
+    for(int i = len - 1;i >= 0;i--)
+    {
+        for(int j = len - 1;j > len - i - 1;j--)
+        {
+            char *cur_ptr = c_ptr_ptr[j];
+            char *nxt_ptr = c_ptr_ptr[j - 1];
+
+            size_t cur_len = strlen(cur_ptr);
+            size_t nxt_len = strlen(nxt_ptr);
+
+            if(is_desc)
+            {
+                if(strcmp(cur_ptr + cur_len - file_len, nxt_ptr + nxt_len - file_len) > 0)
+                {
+                    char *p_tmp = nxt_ptr;
+                    c_ptr_ptr[j - 1] = cur_ptr;
+                    c_ptr_ptr[j] = p_tmp;
+                }
+            }
+            else
+            {
+                if(strcmp(cur_ptr + cur_len - file_len, nxt_ptr + nxt_len - file_len) < 0)
+                {
+                    char *p_tmp = nxt_ptr;
+                    c_ptr_ptr[j - 1] = cur_ptr;
+                    c_ptr_ptr[j] = p_tmp;
+                }
+            }
+        }
+    }
+}
+
+int list_dir(const char *path_ptr, char **dir_ptr_ptr)
+{
+    DIR *d;
+    struct dirent *file;
+    struct stat st;
+    int dir_count = 0;
+
+    if(!(d = opendir(path_ptr)))
+    {
+        printf("open dir %s error\n", path_ptr);
+        return dir_count;
+    }
+
+    while((file = readdir(d)) != NULL)
+    {
+        if(strncmp(file->d_name, ".", 1) == 0)
+        {
+            continue;
+        }
+        char sub_path[PATH_MAX] = {0};
+        sprintf(sub_path, "%s/%s", path_ptr, file->d_name);
+        if(stat(sub_path, &st) == 0 && S_ISDIR(st.st_mode))
+        {
+            dir_count++;
+            *(dir_ptr_ptr + (dir_count - 1)) = strdup(file->d_name);
+        }
+    }
+    closedir(d);
+    return dir_count;
+}
+
+int list_file(const char *path_ptr, char **file_ptr_ptr)
+{
+    DIR *d;
+    struct dirent *file;
+    struct stat st;
+    int file_count = 0;
+
+    if(!(d = opendir(path_ptr)))
+    {
+        printf("open dir %s error\n", path_ptr);
+        return file_count;
+    }
+
+    while((file = readdir(d)) != NULL)
+    {
+        if(strncmp(file->d_name, ".", 1) == 0)
+        {
+            continue;
+        }
+        char sub_path[PATH_MAX] = {0};
+        sprintf(sub_path, "%s/%s", path_ptr, file->d_name);
+        if(stat(sub_path, &st) == 0)
+        {
+            if(S_ISDIR(st.st_mode))
+            {
+                file_count += list_file(sub_path, file_ptr_ptr);
+            }
+            else if(S_ISREG(st.st_mode))
+            {
+                file_count++;
+                *(file_ptr_ptr + (file_count - 1)) = strdup(sub_path);
+            }
+
+        }
+    }
+    closedir(d);
+
+    return file_count;
+}
---
+++ util.h
@@ -0,0 +1,105 @@
+//
+// Created by zhengdongtian on 4/22/19.
+//
+
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <errno.h>
+#include <assert.h>
+#include <ctype.h>
+#include <regex.h>
+
+#define gettidv1() syscall(__NR_gettid)
+#define gettidv2() syscall(SYS_gettid)
+
+/**
+ * system函数内部是调用fork，如果在调用system之前程序分配了1G内存，
+ * fork后子进程以及父进程总过内存就是2G，因为我的程序会分配大量内存，
+ * 并且有好几个线程都要调用system，所以造成内存不必要的大量的浪费，
+ * 所以重新写一个system函数（内部使用vfork）
+ * @param command
+ * @return
+ */
+int v_system(const char *command);
+
+/**
+ * 创建目录以及子目录
+ * @param path_ptr
+ * @return
+ */
+int make_dir(const char *path_ptr);
+
+/**
+ * 调用bz2进行解压
+ * @param src_file_path_ptr
+ * @param dst_file_path_ptr
+ * @return
+ */
+int bz2_decompress(const char *src_file_path_ptr, const char *dst_file_path_ptr);
+
+/**
+ * 去除左侧space
+ * @param output_ptr
+ * @param input_ptr
+ * @return
+ */
+char *l_trim(const char *input_ptr, char *output_ptr);
+
+/**
+ * 去除左侧以及右侧的space
+ * @param input_ptr
+ * @param output_ptr
+ * @return
+ */
+char *a_trim(const char *input_ptr, char *output_ptr);
+
+/**
+ * 提取符合pattern_ptr规则的开始以及结束索引
+ * @param input_ptr
+ * @param pattern_ptr
+ * @param so_ptr
+ * @param eo_ptr
+ * @return
+ */
+int reg_exec(const char *input_ptr, const char *pattern_ptr, int *so_ptr, int *eo_ptr);
+
+/**
+ * 根据ascii排序
+ * @param c_ptr_ptr
+ * @param len
+ * @param file_len
+ * @param is_desc
+ */
+void sort_by_ascii(char **c_ptr_ptr, int len, int file_len, int is_desc);
+
+/**
+ * 查找所有子目录
+ * @param path_ptr
+ * @param dir_ptr_ptr
+ * @return
+ */
+int list_dir(const char *path_ptr,  char **dir_ptr_ptr);
+
+/**
+ * 查找所有文件包括子目录中的的文件
+ * @param path_ptr
+ * @param file_ptr_ptr
+ * @return
+ */
+int list_file(const char *path_ptr, char **file_ptr_ptr);
+
+
+#endif //UTIL_H
